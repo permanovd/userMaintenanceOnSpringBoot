683ad114f7454c5cb2c676adc93681cf91b8b924
permanovd <permanovd@gmail.com>
2019-05-05 22:36:11 +0300

    refs #5: Add forgotten delete and edit actions to api.

--- src/main/java/com/permanovd/user_maintainance/User/infrastructure/IocConfiguration.java
+++ src/main/java/com/permanovd/user_maintainance/User/infrastructure/IocConfiguration.java
@@ -1,9 +1,11 @@
 package com.permanovd.user_maintainance.User.infrastructure;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.slf4j.Logger;
 
 @Configuration
 public class IocConfiguration {
@@ -11,6 +13,11 @@ public class IocConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Logger logger() {
+        return LoggerFactory.getLogger("main");
     }
 }
 
